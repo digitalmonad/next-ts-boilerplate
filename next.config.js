8200b4ee0b60eaeaf84792044e3bc448f9597c41
Pavel Kocman <kocmanpavel@gmail.com>
2022-04-30 19:00:11 +0200

    fix: Fix bug during build + add CI badge

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Since adding .test.tsx files was causing bugs during build, I added this rule for resolution between page and page test files.
+  // https://github.com/vercel/next.js/discussions/34249
+  pageExtensions: ['page.tsx'],
 }
 
 module.exports = nextConfig
--- pages/index.test.tsx
+++ pages/index.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from '@testing-library/react'
-import Home from '../pages/index'
-import '@testing-library/jest-dom'
+import Home from './index.page'
 
 describe('Home', () => {
   it('renders a greeting', () => {
